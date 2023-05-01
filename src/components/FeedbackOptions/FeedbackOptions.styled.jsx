a2d3304b5e163da0b696d644868138ba8a744a5a
Ivan Klimov <klimov128@gmail.com>
2023-05-01 14:40:28 +0300

    Update FeedbackOptions.styled.jsx

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -13,15 +13,15 @@ export const Button = styled.button`
   border-radius: 4px;
   box-shadow: 0px 2px 2px 0px rgba(0, 0, 0, 0.1215686275),
     0px 1px 2px 0px rgba(0, 0, 0, 0.0784313725);
-  cursor: pointer;
   text-transform: capitalize;
   font-weight: 600;
 
   &:hover {
-    background-color: whitesmoke;
+    cursor: pointer;
   }
 
   &:active {
     box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.0384313725);
+    background-color: whitesmoke;
   }
 `;
